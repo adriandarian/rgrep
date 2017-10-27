57ac8d6d8d332dcfd614c12b48caffcd46ab1c20
adriandarian <adarian@ucmerced.edu>
2017-10-27 15:46:14 -0700

    update v2

--- rgrep.c
+++ rgrep.c
@@ -1,167 +1,229 @@
 #include <stdio.h>
-#define MAXSIZE 4096
+	#define MAXSIZE 4096
 
-/**
- * You can use this recommended helper function
- * Returns true if partial_line matches pattern, starting from
- * the first char of partial_line.
- */
-int matches_leading(char *partial_line, char *pattern) {
-  // Implement if desire
-  return 0;
-}
+	char next_char(char *string) {
+	       return *(string + sizeof(char));
+	}
 
-/**
- * You may assume that all strings are properly null terminated
- * and will not overrun the buffer set by MAXSIZE
- *
- * Implementation of the rgrep matcher function
- */
-int pIndex;
-int lIndex;
-int plus;
-int slash;
-int question;
-int rgrep_matches(char *line, char *pattern) {
-    //
-    // Implement me
-    //
-    switch (pattern[pIndex]) {
-      case '+':
-        if (line[lIndex] == pattern[pIndex + 1]) {
-          if (pattern[pIndex + 2] == '\0') {
-            lIndex = 0;
-            pIndex = 0;
-            plus = 1;
-          } else {
-            pIndex++;
-            plus = 2;
-          }
-        } else if ( pattern[pIndex + 1] == '\0') {
-          pIndex++;
-          if (pattern[pIndex] == '\0') {
-            pIndex = 0;
-            lIndex = 0;
-            plus = 1;
-          }
-        } else if (line[lIndex + 1] == pattern[pIndex - 1]) {
-          lIndex++;
-          plus = 2;
-        } else if ((line[lIndex + 1] == '\0') && (line[lIndex] != pattern[pIndex - 1])) {
-          pIndex++;
-          if (line[lIndex] == pattern[pIndex]) {
-            lIndex = 0;
-            pIndex = 0;
-          } else {
-            pIndex = 0;
-            plus = 2;
-          }
-        } else if (line[lIndex + 1] != pattern[pIndex - 1]) {
-          lIndex++;
-          pIndex++;
-          if ((line[lIndex] == pattern[pIndex]) && (pattern[pIndex + 1] == '\0')) {
-            lIndex = 0;
-            pIndex = 0;
-            plus = 1;
-          }
-        } else {
-          pIndex = 0;
-          plus = 0;
-        }
-        if (plus == 1) {
-          lIndex = 0;
-          pIndex = 0;
-          printf("%s\n", line);
-          return 1;
-        } else if (plus == 2) {
-          rgrep_matches(line, pattern);
-        } else {
-          return 0;
-        }
-        break;
-      case '.':
-        if (line[lIndex] != '\n') {
-          pIndex++;
-          lIndex++;
-          rgrep_matches(line, pattern);
-        } else {
-          return 0;
-        }
-        break;
-      case '\\':
-        if (line[lIndex] == pattern[pIndex + 1]) {
-          if (line[lIndex + 1] != '\0') {
-            pIndex++;
-            slash = 1;
-          } else {
-            pIndex = 0;
-          }
-        } else {
-          lIndex++;
-          slash = 1;
-        }
-        if (slash == 1) {
-          rgrep_matches(line, pattern);
-        }
-        break;
-      case '?':
-        if ()
-        break;
-      default:
-        if (pattern[pIndex] == '\0' || pattern[pIndex] == '\n') {
-          pIndex = 0;
-          lIndex = 0;
-          printf("%s\n", line);
-          return 1;
-        } else if (line[lIndex] == '\0' || line[lIndex] == '\n') {
-          pIndex = 0;
-          lIndex = 0;
-          return 0;
-        }else if (line[lIndex] != pattern[pIndex]) {
-          if ( pattern[pIndex + 1] == '?') {
-            pIndex++;
-            rgrep_matches(line, pattern);
-          } else {
-            lIndex++;
-            pIndex = 0;
-            rgrep_matches(line, pattern);
-          }
-        } else if (line[lIndex] == pattern[pIndex]) {
-          lIndex++;
-          pIndex++;
-          rgrep_matches(line, pattern);
-        } else {
-          return 0;
-        }
-        lIndex = 0;
-        pIndex = 0;
-    }
-    return 0;
-}
+	char prev_char(char *string) {
+	       return *(string - sizeof(char));
+	}
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <PATTERN>\n", argv[0]);
-        return 2;
-    }
+	//Accepts char, not pointer
+	int is_operator(char pattern) {
+	       switch(pattern) {
+	              case '.' :
+	                     return 1;
+	              case '\\' :
 
-    /* we're not going to worry about long lines */
-    char buf[MAXSIZE];
+	                     return 2;
+	              case '+' :
+	                     return 3;
+	              case '\?' :
+	                     return 4;
+	              default :
+	                     return 0;
+	       }
+	}
 
-    while (!feof(stdin) && !ferror(stdin)) {
-        if (!fgets(buf, sizeof(buf), stdin)) {
-            break;
-        }
-        if (rgrep_matches(buf, argv[1])) {
-            fputs(buf, stdout);
-            fflush(stdout);
-        }
-    }
+	int plus_modified(char *pattern) {
+	       if(next_char(pattern) == '+')
+	              return 1;
+	       return 0;
+	}
 
-    if (ferror(stdin)) {
-        perror(argv[0]);
-        return 1;
-    }
+	int question_modified(char *pattern) {
+	       if(next_char(pattern)  == '\?')
+	              return 1;
+	       return 0;
+	}
 
-    return 0;
-}
+	int escape_modified(char *pattern) {
+	       if(prev_char(pattern) == '\\')
+
+	              return 1;
+	       return 0;
+	}
+
+	/**
+	* You can use this recommended helper function
+	 * Returns true if partial_line matches pattern, starting from
+	* the first char of partial_line.
+	*/
+
+	int matches_leading(char *line, char *pattern) {
+	       //Check direct match
+	       if(*line == *pattern && !is_operator(*pattern)) {
+	              return 1;
+	       }
+	       //Check wildcard match
+	       if(*pattern == '.' && !escape_modified(pattern)) {
+	              return 1;
+	       }
+	       //Check escaped operator match
+	       if(*line == *pattern && is_operator(*pattern) && escape_modified(pattern)) {
+	              return 1;
+	       }
+	       //Check escape character
+	       if(*pattern == '\\')
+
+	              return matches_leading(line, pattern + sizeof(char));
+	       //Check for absent case of ? operator
+	       if(question_modified(pattern))
+	              return 1;
+	       return 0;
+	}
+
+	/**
+	* You may assume that all strings are properly null terminated
+	 * and will not overrun the buffer set by MAXSIZE
+	 *
+	* Implementation of the rgrep matcher function
+	*/
+	int rgrep_matches(char *line, char *pattern) {
+
+	       static int pattern_depth = 0;
+
+
+
+	       //Base cases
+	       if(*pattern == '\0') {
+	              //Reset pattern for new line
+	              if(*line == '\n') {
+	                     pattern -= pattern_depth * sizeof(char);
+	              }
+	              pattern_depth = 0;
+	              return 1;
+	       }
+
+	       if(*line == '\0')
+	              return 0;
+
+	       //Reset pattern for new line
+	       if(*line == '\n') {
+	              pattern -= pattern_depth * sizeof(char);
+	              pattern_depth -= pattern_depth;
+	              return 0;
+	       }
+
+	       //Check for escape
+	       if((*pattern == '\\') && is_operator(next_char(pattern))) {
+	              pattern += sizeof(char);
+	              pattern_depth++;
+	       }
+
+
+
+	       //Check for match
+	       if(matches_leading(line, pattern)) {
+	              //For + modifier
+	              if(plus_modified(pattern)) {
+	                     int chars_before = 1;
+	                     if(*pattern == *(pattern + 2 * sizeof(char)))
+	                           chars_before = 2;
+	                     //Go to next nonmatching character
+	                     //Stops one before because there is a line increment at the end of rgrep_matches
+	                     while(*(line + chars_before * sizeof(char)) == *pattern && !(*pattern == '.' && !escape_modified(pattern)))
+	                           line += sizeof(char);
+	                     //Handle .+
+	                     if(*pattern == '.' && escape_modified(pattern)) {
+	                            char wildcard = *line;
+	                            while(*(line + chars_before * sizeof(char)) == wildcard)
+	                                  line += sizeof(char);
+	                     }
+	                     if(*pattern == '.' && !escape_modified(pattern)) {
+	                            //while(*(line + chars_before * sizeof(char)) != *(pattern + 2 * sizeof(char)))
+	                            while(!matches_leading(line + chars_before * sizeof(char), pattern + 2 * sizeof(char))){
+	                                  line += sizeof(char);
+	                                  if((*line == '\0' || *line == '\n') && *(pattern + 2 * sizeof(char)) != '\0')
+	                                         return 0;
+	                           }
+	                     }
+	                     //Compensate for the character that the operator took
+	                     pattern += sizeof(char);
+	                     pattern_depth++;
+	              }
+	              //For ? modifier
+	              if(question_modified(pattern)) {
+	                     //
+	                     if (*pattern == '.' && !escape_modified(pattern) && escape_modified(pattern + 3 * sizeof(char)) && *line == *(pattern + 3 * sizeof(char)) && *(line + sizeof(char)) != *(pattern + 3 * sizeof(char))) {
+	                           pattern_depth += 4;
+	                            return rgrep_matches(line + sizeof(char), pattern + 4 * sizeof(char));
+	                     }
+	                     //
+	                     else if(*pattern == '.' && !escape_modified(pattern) && *line == *(pattern + 2 * sizeof(char))) {
+	                           pattern_depth += 2;
+	                            return rgrep_matches(line, pattern + 2 * sizeof(char));
+	                     }
+	                     //
+	                     else if(*pattern == '.' && !escape_modified(pattern) && *line != *(pattern + 2 * sizeof(char))) {
+	                           pattern_depth += 2;
+	                            return rgrep_matches(line + sizeof(char), pattern + 2 * sizeof(char));
+	                     }
+	                     //
+	                     else if(*line == *pattern && *line == *(pattern + 2 * sizeof(char))) {
+	                           pattern_depth += 2;
+	                            return rgrep_matches(line, pattern + 2 * sizeof(char));
+	                     }
+	                     //
+	                     else if(*line == *pattern && next_char(line) == *(pattern + 2 * sizeof(char))){}
+	                     //
+	                     else if(*line == *pattern && next_char(line) != *(pattern + 2 * sizeof(char)) && *line != *(pattern + 2 * sizeof(char)))
+	                           {}//return 0;
+	                     //
+	                     else if(*line != *pattern && *line != *(pattern + 2 * sizeof(char)))
+	                            return 0;
+	                     //
+	                     else if(*line == *pattern && *pattern == '.' && escape_modified(pattern)){}
+	                     //
+	                     else if(!(*pattern == '.' && !escape_modified(pattern)) || *line != *pattern)
+	                           line -= sizeof(char);
+	                     //Compensate for the character that the operator took
+	                     pattern += sizeof(char);
+	                     pattern_depth++;
+	              }
+	              pattern += sizeof(char);
+	              pattern_depth++;
+	       }
+
+	       //Reset the pattern on partial match so that we only get contiguous matches
+	       else if(pattern_depth != 0 && *(pattern - pattern_depth * sizeof(char)) != '\0') {
+	              pattern -= pattern_depth * sizeof(char);
+	              pattern_depth -= pattern_depth;
+	       }
+
+	       //if(*line != '\n')
+	              line += sizeof(char);
+
+
+
+	       //Recurse
+	       return rgrep_matches(line, pattern);
+	}
+
+	int main(int argc, char **argv) {
+	    if (argc != 2) {
+	        fprintf(stderr, "Usage: %s <PATTERN>\n", argv[0]);
+	        return 2;
+	    }
+
+	    /* we're not going to worry about long lines */
+	    char buf[MAXSIZE];
+
+	    while (!feof(stdin) && !ferror(stdin)) {
+	        if (!fgets(buf, sizeof(buf), stdin)) {
+	            break;
+	        }
+	        if (rgrep_matches(buf, argv[1])) {
+	            fputs(buf, stdout);
+	            fflush(stdout);
+	        }
+	    }
+
+	    if (ferror(stdin)) {
+	        perror(argv[0]);
+	        return 1;
+	    }
+
+	    return 0;
+	}
